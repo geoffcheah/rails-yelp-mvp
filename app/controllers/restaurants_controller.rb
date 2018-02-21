bab4b45b0cd5425240d2060f2e210b1a75920c21
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-21 15:03:30 +0000

    added link to add new restaurant. finished writing new and create actions in restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,10 +9,21 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render new_restaurant_path
+    end
+  end
 
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
